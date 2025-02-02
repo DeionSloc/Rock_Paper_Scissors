d604321b3eae38c804784efbb010497b30bbaa64
DeionSloc <95542183+DeionSloc@users.noreply.github.com>
2025-02-01 20:34:54 -0700

    Add new functions
    Added new functions to the script.js file
    Added function for the rock, paper, scissors game
    Added parameters for human score and computer score

--- script.js
+++ script.js
@@ -27,15 +27,52 @@ function getHumanChoice(){
             {
                 return "Paper"
             }   
-        else (humanChoice == "scissors")
+        else if(humanChoice == "scissors")
         {
             return "Scissors"
         }
 }
 
 function playRound(humanChoice, computerChoice){
-
+    if (humanSelection == "Rock" && computerSelection == "Scissors")
+            {
+                humanScore ++;
+                return "Rock beats scissors, you win"
+            }
+            else if (humanSelection == "Paper" && computerSelection == "Rock") {
+                    humanScore ++;
+                    return "Paper beats rock, you win."
+                }
+                else if (humanSelection == "Scissors" && computerSelection == "Paper")
+                {
+                    humanScore ++;
+                    return "Scissors beats paper, you win."    
+                }
+                    else if (humanSelection == "Scissors" && computerSelection == "Rock")
+                        {
+                            computerScore ++;
+                            return "Rock beats scissors, you lose."
+                        }
+                        else if (humanSelection == "Paper" && computerSelection == "Scissors")
+                            {
+                                computerScore ++;
+                                return "Scissors beats paper, you lose."
+                            }
+                            else if (humanSelection == "Rock" && computerSelection == "Paper")
+                                {
+                                    computerScore ++;
+                                    return "Paper beats rock, you lose."
+                                }
+                                else {
+                                    return "It's a draw"
+                                }
 }
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
-console.log(getComputerChoice(3))
-console.log(getHumanChoice())
+// console.log(getComputerChoice(3))
+// console.log(getHumanChoice())
+console.log(playRound(humanSelection, computerSelection));
+console.log("Your score is " + humanScore)
+console.log("The computer's score is " + computerScore)
+
