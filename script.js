1b6c6a3d3b00e531f33738892e4699d5deebdec4
DeionSloc <95542183+DeionSloc@users.noreply.github.com>
2025-02-12 21:33:30 -0700

    Update index.html, styles.css and script.js
    Updated script.js, added functionality to reload the page without user input, updated score tracking and rock, paper, scissors code
    Updated gui using styles.css so its more user friendly. Added a score tracker and modified buttons for rock, paper, scissors
    Updated index.html, added custom messages for winning and losing.

--- script.js
+++ script.js
@@ -1,80 +1,64 @@
-let humanScore = 0;
+const playerScoreDisplay = document.getElementsByClassName("playerScoreDisplay");
+const computerScoreDisplay = document.getElementsByClassName("playerScoreDisplay");
+
+let playerScore = 0;
 let computerScore = 0;
 
-function getComputerChoice(max){
-    if (Math.floor(Math.random() * max) == 0)
-    {
-        return "Rock"
-    }
-        if (Math.floor(Math.random() * max) == 1)
-        {
-            return "Paper"
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementsByClassName("playerDisplay");
+const computerDisplay = document.getElementsByClassName("computerDisplay");
+const resultsDisplay = document.getElementsByClassName("resultsDisplay");
+
+function playGame(playerChoice){
+        const computerChoice = choices[Math.floor(Math.random() *3)];
+        let result= "";
+        if(playerScore === 5 || computerScore === 5){
+            if(playerScore > computerScore){
+                alert("We beat them back, this time. Hurry, prepare for the next assault, they don't give up so easily.")
+            }
+            else{
+                alert("Retreat, retreat! Our last stronghold has fallen, regroup at the rendezvous!")
+            }
+            location.reload();
         }
-        else (Math.floor(Math.random() * max) == 2)
-    {
-        return"Scissors"
-    }
-}
+        if (playerChoice === computerChoice){
+            result = "We got em, we got em! We can hold em here until backup arrives.";
+        }
+        else{
+            switch(playerChoice){
+                case "rock":
+                    result = (computerChoice === "scissors") ? "Congrats, you've held them back this time" : "They got through our defense, give it another try soldier!"
+                    break;
+                case "paper":
+                    result = (computerChoice === "rock") ? "Congrats, you've held them back this time" : "They got through our defense, give it another try soldier!"
+                    break;
+                case "scissors":
+                    result = (computerChoice === "paper") ? "Congrats, you've held them back this time" : "They got through our defense, give it another try soldier!"
+                    break;
+            }
+        }
+        playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+        computerDisplay.textContent = `Computer: ${computerChoice}`;
+        resultsDisplay.textContent = result;
 
-// function getHumanChoice(){
-//    let humanChoice = prompt("Selcect rock, paper, or scissors")
+        switch(result){
+            case "Congrats, you've held them back this time":
+                playerScore++;
+                document.querySelector(".playerScoreDisplay").innerHTML = playerScoreDisplay.textContent = playerScore;
+                break;
+            case "They got through our defense, give it another try soldier!":
+                computerScore++;
+                document.querySelector(".computerScoreDisplay").innerHTML = computerScoreDisplay.textContent = computerScore;
+                break;
+        }
 
-//     if (humanChoice == "rock")
-//         {
-//             return "Rock"
-//         }
-//         if (humanChoice == "paper")
-//             {
-//                 return "Paper"
-//             }   
-//         else if(humanChoice == "scissors")
-//         {
-//             return "Scissors"
-//         }
-// }
-
-function playGame(){
-        return function playRound(humanChoice, computerChoice){
-            if (humanSelection == "Rock" && computerSelection == "Scissors")
-                    {
-                        humanScore ++;
-                        return "Rock beats scissors, you win"
-                    }
-                    else if (humanSelection == "Paper" && computerSelection == "Rock") {
-                            humanScore ++;
-                            return "Paper beats rock, you win."
-                        }
-                        else if (humanSelection == "Scissors" && computerSelection == "Paper")
-                        {
-                            humanScore ++;
-                            return "Scissors beats paper, you win."    
-                        }
-                            else if (humanSelection == "Scissors" && computerSelection == "Rock")
-                                {
-                                    computerScore ++;
-                                    return "Rock beats scissors, you lose."
-                                }
-                                else if (humanSelection == "Paper" && computerSelection == "Scissors")
-                                    {
-                                        computerScore ++;
-                                        return "Scissors beats paper, you lose."
-                                    }
-                                    else if (humanSelection == "Rock" && computerSelection == "Paper")
-                                        {
-                                            computerScore ++;
-                                            return "Paper beats rock, you lose."
-                                        }
-                                        else {
-                                            return "It's a draw"
-                                        }
-    }        
-}
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-// console.log(getComputerChoice(3))
-// console.log(getHumanChoice())
-console.log(playGame()(humanSelection, computerSelection));
-console.log("Your score is " + humanScore)
-console.log("The computer's score is " + computerScore)
+        // Add this to get the text to display when then function completes
+        document.querySelector(".resultsDisplay").innerHTML = resultsDisplay.textContent;
+        document.querySelector(".playerDisplay").innerHTML = playerDisplay.textContent;
+        document.querySelector(".computerDisplay").innerHTML = computerDisplay.textContent;
+        // document.querySelector(".playerScoreDisplay").innerHTML = playerScoreDisplay.textContent;
+        // document.querySelector(".computerScoreDisplay").innerHTML = computerScoreDisplay.textContent;
+        // alert(playerScore);
+        // alert(computerScore);
+};
 
